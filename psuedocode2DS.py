d7c95aecbc55bd128c71ec71f5e855a72e4024e8
becks2424 <35306213+becks2424@users.noreply.github.com>
2018-02-01 11:10:25 -0600

    Add files via upload

---
+++ psuedocode2DS.py
@@ -0,0 +1,23 @@
+#Reebcca Menda
+#Lab2
+#Update
+#I UNDERSTOOD THE INSTRUCTIONS WRONG THEREFORE RE-DID IT
+
+import time
+from random import
+
+
+class nurse:
+    def __init__(random, name, self):
+        self.random = random
+        self.name = name
+
+class patient:
+    def __init__(name, location, self):
+        self.name = name
+        self.location = location
+
+class doctor:
+    def __init__(name,self):
+        self.name = name
+        self.random = radient (15,20)
