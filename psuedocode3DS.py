6a652c7e8fdb7f86ddfb9966efb078abdc0258ed
becks2424 <35306213+becks2424@users.noreply.github.com>
2018-02-05 07:45:05 -0600

    Add files via upload

---
+++ psuedocode3DS.py
@@ -0,0 +1,50 @@
+#Reebcca Menda
+#Lab3
+
+
+import time
+from random import
+
+def main():
+
+waitingRoom = []
+triageRoom = []
+examRoom = []
+
+examRoomsize = 6
+doctors = 6
+
+names = ["Joey", "Bobby", "Sueanne", "Loretta", "Grant", "Jenny", "Billy"\
+         "Cletus", "Hunter"]
+
+def callNurse:
+    triageRoom.append (waitingRoom.pop(0))
+    sort(triageRoom, key = patient.triageNumber)
+
+    
+
+##
+##class patientName:
+##        patient(name [random.randInt(0,len(names)-1]\
+##                           + " " + names [random.randInt(0,len(names)-1])
+        #We'll be able to get names of the patients from the list we made above\
+    #was trying out something different
+                      
+class patient:
+    def __init__(name, location, self):
+        self.triageNumber = random.randInt(0, 100)
+        self.name = name [random.randInt(0,len(names)-1]\
+                           + " " + names [random.randInt(0,len(names)-1]
+        self.arrivalTime = time
+        self.treatmentTime = random.randInt (15,20)
+
+    def exit (self):
+            pass
+
+##class doctor:
+##    def __init__(name,self):
+##        self.name = name
+##        self.random = random (15,20)
+
+
+main()
